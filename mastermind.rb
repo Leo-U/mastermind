0f410f8ca6ad23958690002d8bbdd8522b3b3bd8
Joel Howard <joelhoward2@gmail.com>
2022-11-22 06:11:48 -0500

    edit readme, implement apparent full playability (pre-refactor)

--- mastermind.rb
+++ mastermind.rb
@@ -6,9 +6,17 @@ end
 
 class Creator
   @@colors = {red: "🔴", orange: "🟠", yellow: "🟡", green: "🟢", blue: "🔵", brown: "🟤", black: "⚫", white: "⚪"}
-  def self.set_code
+
+  def self.shuffle_code
     @@code = @@colors.shuffle.first(4).to_h
+  end
+
+  def self.set_code
     @@code_strs = @@code.keys.map {|el| el = el.to_s}
+  end
+
+  def self.code=(arg)
+    @@code = arg
   end
 
   def self.code
@@ -52,16 +60,12 @@ class Guesser
 
   def self.play_game
     i = 11
-    puts "Enter 4 of the color names to guess the code, e.g. 'red orange yellow green'. The code will not repeat colors, but Guesser may repeat colors in their guess. The clue is displayed in brackets for each turn -- the first number is the number of colors that are correct but in the right position, and the second number is the number of colors that are correct but in the wrong position."
+    puts "Enter 4 of the color names to guess the code, e.g. 'red orange yellow green'. The code will not repeat colors, but Guesser may repeat colors in their guess. The 2-digit clue is displayed in brackets for each turn — the first digit is the number of colors that are correct and in the right position, and the second digit is the number of colors that are correct but in the wrong position."
     puts Creator.colors.values.join' '
     12.times do
       puts "Enter next choice." if i < 11
       Board.reset_clue
-      if ActivePlayer.chosen == "guesser"
-        @@current_guess = gets.split
-      elsif ActivePlayer.chosen == "creator"
-        # @@current_guess = 
-      end
+      @@current_guess = gets.split
       Guesser.play_round
       if Guesser.current_guess == Creator.strs
         puts "Guesser wins!"
@@ -86,76 +90,34 @@ class Computer
     set = set.map {|el| el.to_s}.map do |el|
         el.gsub("1","red ").gsub("2","orange ").gsub("3","yellow ").gsub("4","green ").gsub("5","blue ").gsub("6","brown ").gsub("7","black ").gsub("8","white ").rstrip
     end
-
     Guesser.current_guess = "red red orange orange".split
-    p Guesser.current_guess
-
-  12.times do
-    Board.reset_clue
-    if Guesser.current_guess == Creator.strs
-      puts "Guesser wins!"
-      puts Creator.code.values.join' '
-      break
-    end
-    push_me = []
-    Guesser.play_round
-    clue_copy = Board.clue
-    p set.length
-    set.each do |el|
+    i = 11
+    12.times do
+      sleep(2)
+      puts "The computer guesses '#{Guesser.current_guess.join' '}'."
       Board.reset_clue
-      code_copy = Creator.strs
-      Creator.strs = el.split
+      if Guesser.current_guess == Creator.strs
+        puts "Computer wins!"
+        puts Creator.code.values.join' '
+        break
+      end
+      push_me = []
       Guesser.play_round
-      push_me.push(el) if Board.clue == clue_copy
-      Creator.strs = code_copy
+      p Board.clue
+      puts "Attempts remaining: #{i}"
+      clue_copy = Board.clue
+      set.each do |el|
+        Board.reset_clue
+        code_copy = Creator.strs
+        Creator.strs = el.split
+        Guesser.play_round
+        push_me.push(el) if Board.clue == clue_copy
+        Creator.strs = code_copy
+      end
+      set = push_me
+      Guesser.current_guess = set[0].split
+      i -= 1
     end
-    set = push_me if push_me.length > 0
-    Guesser.current_guess = set[0].split if push_me.length > 0
-    p push_me.length
-    p set[0]
-
-    p Guesser.current_guess
-
-  end
-
-  
-
-
-
-    # 12.times do
-    #   Guesser.play_round
-    #   if Guesser.current_guess == Creator.strs
-    #     puts "Guesser wins!"
-    #     puts Creator.code.values.join' '
-    #     break
-    #   end
-    #   @@current_clue = Board.clue
-    #   Board.reset_clue
-    #   new_arr = []
-    #   set.each do |el|
-    #     Guesser.current_guess = el.split
-    #     Guesser.play_round
-    #     new_arr.push(el) if Board.clue == @@current_clue
-    #     Board.reset_clue
-    #   end
-    #   set = new_arr
-    #   Guesser.current_guess = set[0].split
-    # end
-
-    # set = set.filter do |el|
-    #   Guesser.current_guess = 
-    # end
-  end
-end
-
-class ActivePlayer
-  def self.gets_active_player
-    puts "Do you want to be the code creator or the code guesser? Enter 'creator' or 'guesser'."
-    @@chosen = gets.chomp
-  end
-  
-  def self.chosen
-    @@chosen
   end
 end
 
@@ -175,8 +137,35 @@ class Board
   end
 end
 
-Creator.set_code
-Computer.crack_code
-# Creator.set_code
-# ActivePlayer.gets_active_player
-# Guesser.play_game
+class Game
+  def self.choose_side
+    puts "Do you want to be the code guesser or the code chooser? Type 'guesser' or 'chooser'."
+    @@active_player = gets.chomp
+  end
+
+  def self.play_game
+    choose_side
+    if @@active_player == "chooser"
+      puts "To choose the code that the computer must guess, enter a space-separated, non-repeating list (e.g. 'red white blue yellow') of these colors:"
+      puts "#{Creator.colors.values.join' '}"
+      puts "For each turn, the computer will use the 2-digit clue you'll see in brackets — the first digit is the number of colors that are correct and in the right position, and the second digit is the number of colors that are correct but in the wrong position."
+      code = gets.split
+      code = code.map { |el| el.to_sym}
+      push_me = []
+      Creator.colors.each_pair do | key, value|
+        if code.include?(key)
+          push_me.push([key, value])
+        end
+      end
+      Creator.code = push_me.to_h
+      Creator.set_code
+      Computer.crack_code
+    elsif @@active_player == "guesser"
+      Creator.shuffle_code
+      Creator.set_code
+      Guesser.play_game
+    end
+  end
+end
+
+Game.play_game
