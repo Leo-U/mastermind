f9cebc226af01d72b2c2b0112665bc2ad9b219df
Joel Howard <joelhoward2@gmail.com>
2022-11-23 19:46:34 -0500

    light refactor, edit readme

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ will write pseudocode for the crack_code method here. will also include gameplay
 current plan:
 
 1) refactor for readability.
+  A) Go thru each line and try and make your code shorter.
+  B) Make your code more modular. rn there's a lot of dependency between classes. DO I accomplish this by joining classes together? Sorting them more effectively?
 
 2) add typo handling.
--- mastermind.rb
+++ mastermind.rb
@@ -44,21 +44,21 @@ class Guesser
     iterate_me = @@current_guess.dup
     iterate_me.each_with_index do |el, i|
       if el == Creator.strs[i]
-        Board.incr_clue(0)
+        Clue.incr_clue(0)
         push_me.push(el)
       end
     end
     iterate_me.delete_if { |el| push_me.include?(el)}
-    iterate_me.uniq.each { |el| Board.incr_clue(1) if Creator.strs.include?(el)}
+    iterate_me.uniq.each { |el| Clue.incr_clue(1) if Creator.strs.include?(el)}
   end
 
   def self.play_game
     i = 11
-    puts "Enter 4 of the color names to guess the code, e.g. 'red orange yellow green'. The code will not repeat colors, but Guesser may repeat colors in their guess. The 2-digit clue is displayed in brackets for each turn — the first digit is the number of colors that are correct and in the right position, and the second digit is the number of colors that are correct but in the wrong position."
+    puts "Enter 4 of the color names to guess the code, e.g. 'red orange yellow green'. The code will not repeat colors, but you may repeat colors in your guess. The 2-digit clue is displayed in brackets for each turn — the first digit is the number of colors that are correct and in the right position, and the second digit is the number of colors that are correct but in the wrong position."
     puts Creator.colors.values.join' '
     12.times do
       puts "Enter next choice." if i < 11
-      Board.reset_clue
+      Clue.reset_clue
       @@current_guess = gets.split
       Guesser.play_round
       if Guesser.current_guess == Creator.strs
@@ -68,7 +68,7 @@ class Guesser
       end
       puts "Attempts remaining: #{i}"
       if i >= 1
-        p Board.clue
+        p Clue.clue
       else 
         puts "Guesser failed. The code was #{Creator.code.values.join' '} ."
       end
@@ -85,13 +85,11 @@ class Computer
         el.gsub("1","red ").gsub("2","orange ").gsub("3","yellow ").gsub("4","green ").gsub("5","blue ").gsub("6","brown ").gsub("7","black ").gsub("8","white ").rstrip
     end
     Guesser.current_guess = "red red orange orange".split
-    p Guesser.current_guess
-    p Creator.strs
     i = 11
     12.times do
       sleep(1.5)
       puts "The computer guesses '#{Guesser.current_guess.join' '}'."
-      Board.reset_clue
+      Clue.reset_clue
       if Guesser.current_guess == Creator.strs
         puts "Computer wins!"
         puts Creator.code.values.join' '
@@ -99,15 +97,15 @@ class Computer
       end
       push_me = []
       Guesser.play_round
-      p Board.clue
+      p Clue.clue
       puts "Attempts remaining: #{i}"
-      clue_copy = Board.clue
+      clue_copy = Clue.clue
       set.each do |el|
-        Board.reset_clue
+        Clue.reset_clue
         code_copy = Creator.strs
         Creator.strs = el.split
         Guesser.play_round
-        push_me.push(el) if Board.clue == clue_copy
+        push_me.push(el) if Clue.clue == clue_copy
         Creator.strs = code_copy
       end
       set = push_me
@@ -117,7 +115,7 @@ class Computer
   end
 end
 
-class Board
+class Clue
   @@clue = [0, 0]
 
   def self.incr_clue(index)
